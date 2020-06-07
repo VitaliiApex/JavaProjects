6f0d35bb6d58fe3ebbfa0b1feb2105579d8a8787
VitaliiApex <Vitalii.Apex@gmail.com>
2020-06-07 00:59:03 -0400

    MyFirstCommit

---
+++ src/DataTypes/DataTypes.java
@@ -0,0 +1,19 @@
+package DataTypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+
+//    boolean a = “true”;      boolean a = true;
+//    byte b  = 129;           byte b = 127;
+//    short c = 25f;           short c = 25;
+//    long e = 6456567774;     long e = 6456567774L;
+//    int t = -8798954356;     int t = -1798954356;
+//    double p = ‘-6’;         double p = -6;
+//    float s = 4.89;          float s = 4.89f;
+//    char l = “y”;            char l = 'y';
+
+
+}
+}
+
---
+++ src/DisplayNumbersInConsole/DisplayNumbersInConsole.java
@@ -0,0 +1,13 @@
+package DisplayNumbersInConsole;
+
+public class DisplayNumbersInConsole {
+
+        public static void main(String[] args) {
+
+            System.out.println("Display the numbers 1 to 10 in the console");
+            for (int a = 1; a <= 10; a++) {
+                System.out.print(a +" ");
+            } System.out.println("\n");
+
+        }
+}
