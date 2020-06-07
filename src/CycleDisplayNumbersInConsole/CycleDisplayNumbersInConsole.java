e00d2b03f3a7860d132e2a4d0732dcd61469d43e
VitaliiApex <Vitalii.Apex@gmail.com>
2020-06-07 01:05:45 -0400

    MyCommit2

---
+++ src/CycleDisplayNumbersInConsole/CycleDisplayNumbersInConsole.java
@@ -0,0 +1,9 @@
+package CycleDisplayNumbersInConsole;
+
+public class CycleDisplayNumbersInConsole {
+    public static void main(String[] args) {
+        System.out.println("Hello Git-Hub");
+
+
+    }
+}
