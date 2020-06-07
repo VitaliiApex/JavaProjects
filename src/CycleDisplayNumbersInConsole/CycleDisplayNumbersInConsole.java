515d1262184cdeb7a4568f3448a9aedb8ecbe533
VitaliiApex <Vitalii.Apex@gmail.com>
2020-06-07 01:09:23 -0400

    ProjectDone

--- src/CycleDisplayNumbersInConsole/CycleDisplayNumbersInConsole.java
+++ src/CycleDisplayNumbersInConsole/CycleDisplayNumbersInConsole.java
@@ -1,9 +1,16 @@
 package CycleDisplayNumbersInConsole;
 
 public class CycleDisplayNumbersInConsole {
-    public static void main(String[] args) {
-        System.out.println("Hello Git-Hub");
 
+    public static void main(String[] args) {
+
+        System.out.println("Display the numbers 1 to 10 in the console through cycle");
+
+        int circleCounter = 1;
+        do {
+            System.out.println( + circleCounter);
+            circleCounter = circleCounter + 1;
+        } while (circleCounter <= 10);
 
     }
 }
