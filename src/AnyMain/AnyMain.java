91d08c13dba235471e6be51373ee86054f04b060
VitaliiApex <Vitalii.Apex@gmail.com>
2020-07-18 16:46:52 -0400

    ProjectDone

--- src/AnyMain/AnyMain.java
+++ src/AnyMain/AnyMain.java
@@ -3,7 +3,7 @@ package AnyMain;
 public class AnyMain {
 
     public static void main(String[] args) {
-        System.out.println(0.1d);
+        System.out.println();
 
 
 
---
+++ src/P010_L007_Rectangle/P010_L007_Rectangle.java
@@ -0,0 +1,19 @@
+package P010_L007_Rectangle;
+
+import java.awt.*;
+
+public class P010_L007_Rectangle {
+    public static void main(String[] args) {
+
+        Rectangle ab = new Rectangle ();
+
+        ab.RectangleLength = "довжина";
+        ab.RectangleWidth = "ширина";
+
+        System.out.println("Площа прямокутника" + " = тут буде площа");
+
+        System.out.println("Периметр прямокутника" + " = тут буде периметр");
+
+
+    }
+}
