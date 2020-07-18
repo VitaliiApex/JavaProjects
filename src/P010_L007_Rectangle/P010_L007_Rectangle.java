9dcc1df1a5604386f211a97c56d5f8e9a697d0a6
VitaliiApex <Vitalii.Apex@gmail.com>
2020-07-18 17:05:24 -0400

    ProjectDone

--- src/P010_L007_Rectangle/P010_L007_Rectangle.java
+++ src/P010_L007_Rectangle/P010_L007_Rectangle.java
@@ -7,13 +7,35 @@ public class P010_L007_Rectangle {
 
         Rectangle ab = new Rectangle ();
 
-        ab.RectangleLength = "довжина";
-        ab.RectangleWidth = "ширина";
+        public class Rectangle {
+            private static final int width;
+            private static final int length;
+            int width = 0;
+            int length = 0;
 
-        System.out.println("Площа прямокутника" + " = тут буде площа");
+            public Rectangle() {
 
-        System.out.println("Периметр прямокутника" + " = тут буде периметр");
+            }
 
+            public Rectangle(int width, int length) {
+                Rectangle.width = width;
+                Rectangle.length = length;
+            }
+            public void areaOfRectangle() {
+                int area = width * length;
+                System.out.println("Площа прямокутника = "+ area);
+            }
+
+            public void perimeterOfRectangle() {
+                int perimeter = 2*(width+length);
+                System.out.println("Периметр прямокутника = " + perimeter);
+            }
+
+            public String toString() {
+                return "Rectangle{" +
+                        "width=" + width +
+                        ", length=" + length +
+                        '}';
 
     }
 }
